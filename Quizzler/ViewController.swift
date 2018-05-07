3b7f9b347fe4cd7975b473c990b6e0f3b4122185
hoffmantim <hoffmant@hilbertk12.org>
2018-05-07 06:53:24 -0500

    Finished quiz elements, UI elements including score, progress #, progress bar. Added UI Alert to restart quiz.

--- Quizzler/ViewController.swift
+++ Quizzler/ViewController.swift
@@ -13,18 +13,28 @@ class ViewController: UIViewController {
     //Place your instance variables here
     let allQuestions = QuestionBank()
     var pickedAnswer : Bool = false
+    var questionNumber : Int = 0
+    var currentScore : Int = 0
+    var progressBarSize : CGFloat = 0.0
+    
+    
+    
     
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var scoreLabel: UILabel!
     @IBOutlet var progressBar: UIView!
     @IBOutlet weak var progressLabel: UILabel!
+    @IBOutlet weak var progressBarTotalLength: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let firstQuestion = allQuestions.list[0]            //could I use arc4random with limit to pick a random question here?
-        questionLabel.text = firstQuestion.questionText     // sets the label of UIlabel to text of first question
+        //could I use arc4random with limit to pick a random question here? It would have to keep track of used questions.
         
+    
+        nextQuestion()
+        
+       
     }
 
 
@@ -36,33 +46,61 @@ class ViewController: UIViewController {
             pickedAnswer = false
         }
         checkAnswer()
+        questionNumber += 1
+       // progressBarSize += (progressBarTotalLength.frame.size.width / CGFloat(13 * questionNumber))
+        nextQuestion()
             
     }
     
     
     func updateUI() {
-      
+        questionLabel.text = allQuestions.list[questionNumber].questionText
+        progressLabel.text = "\(questionNumber + 1) / \(allQuestions.list.count)"
+        scoreLabel.text = "Score: \(currentScore)"
+        progressBar.frame.size.width = (view.frame.size.width / CGFloat(allQuestions.list.count)) * CGFloat(questionNumber + 1)
+        
     }
     
 
     func nextQuestion() {
         
-    }
-    
-    
-    func checkAnswer() {
-        let correctAnswer = allQuestions.list[0].answer
-        
-        if pickedAnswer == correctAnswer {
-            print("you got it!")
+        if questionNumber <= (allQuestions.list.count-1) {
+           updateUI()
         } else {
-            print("wrong")
+            let alert = UIAlertController(title: "End of Quiz", message: "Do you want to start over?", preferredStyle: .alert)
+            
+            let restartAction = UIAlertAction(title: "Restart", style: .default) { (UIAlertAction) in
+                self.startOver()
+            }
+            
+            alert.addAction(restartAction)
+            
+            present(alert, animated: true, completion: nil)
         }
     }
     
     
+    func checkAnswer() {
+        let correctAnswer = allQuestions.list[questionNumber].answer
+        
+        if pickedAnswer == correctAnswer {
+            print("you got it!")
+            currentScore += 1
+            
+            
+        } else {
+            print("wrong")
+        }
+        
+        
+    }
+    
+    
     func startOver() {
-       
+        questionNumber = 0
+        progressBarSize = 0.0
+        currentScore = 0
+        nextQuestion()
     }
     
 
